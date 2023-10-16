dea0a930aaf5ad8ebd807d9eb627a9552e0c5f70
Andrew Lai <the.andrew.lai@gmail.com>
2023-10-15 22:37:32 -0700

    Added History API base and css styling

--- Personal/main.js
+++ Personal/main.js
@@ -1,24 +1,57 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const contentContainers = document.querySelectorAll('.content');
+const navLinks = document.querySelectorAll('a');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function showContent(id) {
+  contentContainers.forEach((content) => {
+    content.style.display = 'none';
+  });
 
-setupCounter(document.querySelector('#counter'))
+  // make last page hide
+
+  // show content
+  const content = document.getElementById(id + '-content');
+  if (content) {
+    content.style.display = 'block';
+  }
+}
+
+// Handle navigation link clicks
+navLinks.forEach((navLink) => {
+
+    const id = navLink.id.replace(/-button$/, '');
+    console.log(id);
+
+    navLink.addEventListener('click', (event) => {
+
+        event.preventDefault(); // Prevent the default link behavior (full page refresh)
+
+        if(id=="home"){
+            showContent(id);
+            history.pushState({ page: id }, '', `/`);
+        }
+        else{
+            // const id = event.target.getAttribute('href').substring(1); // Get the link's target ID
+            showContent(id);
+
+            // Update the URL using the History API
+            history.pushState({ page: id }, '', `/${id}`);
+        }
+    });
+});
+
+// Handle the popstate event to update content when back/forward buttons are used
+window.addEventListener('popstate', (event) => {
+  const state = event.state;
+  if (state && state.page) {
+    showContent(state.page);
+  }
+});
+
+// Check the URL on initial page load and display the corresponding content
+window.addEventListener('DOMContentLoaded', () => {
+  const path = window.location.pathname.substring(1); // Remove the leading slash
+  if (path) {
+    showContent(path);
+  }
+});
+
